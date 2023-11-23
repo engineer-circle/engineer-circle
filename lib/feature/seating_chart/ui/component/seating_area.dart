8b2dcb63492a5ee64f20efe111f43bb401c0e08c
h.kuwabara <h.kuwabara1103@gmail.com>
2023-11-23 19:27:32 +0900

    SeatingAreaを動的に変化するように修正

--- lib/feature/seating_chart/ui/component/seating_area.dart
+++ lib/feature/seating_chart/ui/component/seating_area.dart
@@ -1,4 +1,4 @@
-import 'package:collection/collection.dart';
+import 'package:engineer_circle/feature/admin/create_seating_chart/state/component_state/create_seat.dart';
 import 'package:engineer_circle/feature/seating_chart/state/component_state/seat_group.dart';
 import 'package:engineer_circle/feature/seating_chart/ui/component/horizontal_seating_layout.dart';
 import 'package:engineer_circle/feature/seating_chart/ui/component/vertical_seating_layout.dart';
@@ -14,61 +14,58 @@ class SeatingArea extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final seats = [1, 2, 3, 4];
     return Column(
       children: [
         const Text(
           'Front',
           style: TextStyle(fontSize: 24),
         ),
-        ...seats.expandIndexed<Widget>((index, number) {
-          return listColumn();
-        }),
+        ...seatGroupMatrix.map<Widget>(
+          (rowSeatGroup) {
+            return rowSeatWidget(rowSeatGroup);
+          },
+        ).toList(),
       ],
     );
   }
 
-  List<Widget> listColumn() {
+  Widget rowSeatWidget(List<SeatGroup> rowSeatGroup) {
     final int sideSeatCounts = 3;
     final int seatId = 1;
 
-    List<Widget> _listColumn = [];
-    for (int j = 0; j < 4; j++) {
-      List<Widget> _listRow = [];
-      for (int i = 0; i < 3; i++) {
-        _listRow.add(
-          (i % 2 == j % 2)
-              ? Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: HorizontalSeatingLayout(
-                    tableId: 1,
-                    firstSeatId: i,
-                    sideSeatCounts: sideSeatCounts,
-                    onSeatSelected: (seatId) {
-                      print('@@@@@Tap: $i, seatId: $seatId');
-                    },
-                  ),
-                )
-              : Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: VerticalSeatingLayout(
-                    tableId: 2,
-                    firstSeatId: i,
-                    sideSeatCounts: sideSeatCounts,
-                    onSeatSelected: (seatId) {
-                      print('@@@@@Tap: $i, seatId: $seatId');
-                    },
-                  ),
-                ),
-        );
+    List<Widget> _listRow = rowSeatGroup.map<Widget>((seatGroup) {
+      switch (seatGroup.seatOrientation) {
+        case SeatOrientation.horizontal:
+          return Padding(
+            padding: const EdgeInsets.all(16),
+            child: HorizontalSeatingLayout(
+              tableId: 1,
+              firstSeatId: 1,
+              sideSeatCounts: sideSeatCounts,
+              onSeatSelected: (seatId) {
+                print('@@@@@Tap: 1, seatId: $seatId');
+              },
+            ),
+          );
+        case SeatOrientation.vertical:
+          return Padding(
+            padding: const EdgeInsets.all(16),
+            child: VerticalSeatingLayout(
+              tableId: 2,
+              firstSeatId: 1,
+              sideSeatCounts: sideSeatCounts,
+              onSeatSelected: (seatId) {
+                print('@@@@@Tap: 1, seatId: $seatId');
+              },
+            ),
+          );
+        default:
+          return Container(); // または、デフォルトのウィジェットを返す
       }
-      _listColumn.add(
-        Row(
-          children: _listRow,
-        ),
-      );
-    }
+    }).toList();
 
-    return _listColumn;
+    return Row(
+      children: _listRow,
+    );
   }
 }
