df027713026bba1f5781fe3c6259fc9562182123
h.kuwabara <h.kuwabara1103@gmail.com>
2023-11-30 21:40:05 +0900

    初期化処理を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:engineer_circle/app/app.dart';
+import 'package:engineer_circle/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async {
+  // 設定の初期化を待たせる
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Firebase初期化
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     const ProviderScope(
       overrides: [],
