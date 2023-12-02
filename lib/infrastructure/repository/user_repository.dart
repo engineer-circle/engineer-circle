f3be0c2fe60f5faf6e07d05ca5c063e7084a961b
h.kuwabara <h.kuwabara1103@gmail.com>
2023-12-03 00:11:10 +0900

    UserRepositoryにプロフィール更新のメソッドを追加

--- lib/infrastructure/repository/user_repository.dart
+++ lib/infrastructure/repository/user_repository.dart
@@ -1,19 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:engineer_circle/feature/profile/state/component_state/user.dart';
 import 'package:engineer_circle/infrastructure/remote/firebase.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final profileRepositoryProvider = Provider(
-  (ref) => ProfileRepository(
+final userRepositoryProvider = Provider(
+  (ref) => UserRepository(
     firestore: ref.watch(firebaseFirestoreProvider),
   ),
 );
 
-class ProfileRepository {
-  ProfileRepository({
+class UserRepository {
+  UserRepository({
     required this.firestore,
   });
 
   final FirebaseFirestore firestore;
 
   static const usersCollectionName = 'users';
+
+  Future<void> updateProfile(
+    String uid,
+    User user,
+  ) async {
+    final userRef = firestore.collection(usersCollectionName);
+    await userRef.doc(uid).set(user.toJson());
+  }
 }
