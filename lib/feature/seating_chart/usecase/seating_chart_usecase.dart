ec910ddc97f7ce075513120f8cf4ea9853784a57
h.kuwabara <h.kuwabara1103@gmail.com>
2023-12-07 09:19:47 +0900

    UseCaseにUser一覧取得処理を追加

--- lib/feature/seating_chart/usecase/seating_chart_usecase.dart
+++ lib/feature/seating_chart/usecase/seating_chart_usecase.dart
@@ -1,29 +1,42 @@
+import 'package:collection/collection.dart';
 import 'package:engineer_circle/domain/seat_group.dart';
+import 'package:engineer_circle/domain/user.dart';
 import 'package:engineer_circle/feature/seating_chart/state/component_state/seat_group_view_property.dart';
 import 'package:engineer_circle/feature/seating_chart/state/seating_chart_state.dart';
 import 'package:engineer_circle/infrastructure/repository/seating_chart_repository.dart';
+import 'package:engineer_circle/infrastructure/repository/user_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final seatingChartUseCaseProvider = Provider(
   (ref) => SeatingChartUseCase(
-    repository: ref.watch(seatingChartRepositoryProvider),
-  ),
+      seatingChartRepository: ref.watch(seatingChartRepositoryProvider),
+      userRepository: ref.watch(userRepositoryProvider)),
 );
 
 class SeatingChartUseCase {
   SeatingChartUseCase({
-    required this.repository,
+    required this.seatingChartRepository,
+    required this.userRepository,
   });
 
-  final SeatingChartRepository repository;
+  final SeatingChartRepository seatingChartRepository;
+  final UserRepository userRepository;
 
   Future<SeatingChartStateSuccess> getLatest() async {
-    final seatingChart = await repository.getLatest();
+    final seatingChart = await seatingChartRepository.getLatest();
+
+    final userIds = seatingChart.seats
+        .expand((seatGroup) => seatGroup.seats
+                .where((seat) => seat.userId != null) // nullでないuserIdをフィルタリング
+                .map((seat) => seat.userId!) // userIdを抽出
+            )
+        .toList();
+    final users = await userRepository.getWhereInUsers(userIds);
 
     final seatGroupMatrix = seatingChart.seats
         .fold<List<List<SeatGroupViewProperty>>>([],
             (previousValue, seatGroup) {
-      final value = _toSeatGroupViewProperty(seatGroup);
+      final value = _toSeatGroupViewProperty(seatGroup, users);
       if (previousValue.isEmpty ||
           previousValue.last.last.row != seatGroup.row) {
         previousValue.add([value]);
@@ -33,7 +46,7 @@ class SeatingChartUseCase {
       return previousValue;
     });
 
-    final seatTitles = await repository.getTitles();
+    final seatTitles = await seatingChartRepository.getTitles();
 
     return SeatingChartStateSuccess(
       seatGroupMatrix: seatGroupMatrix,
@@ -42,7 +55,10 @@ class SeatingChartUseCase {
     );
   }
 
-  SeatGroupViewProperty _toSeatGroupViewProperty(SeatGroup seatGroup) {
+  SeatGroupViewProperty _toSeatGroupViewProperty(
+    SeatGroup seatGroup,
+    List<User> users,
+  ) {
     return SeatGroupViewProperty(
       groupId: seatGroup.groupId,
       row: seatGroup.row,
@@ -53,7 +69,8 @@ class SeatingChartUseCase {
           return SeatViewProperty(
             seatId: seat.seatId,
             isSeated: seat.isSeated,
-            user: null,
+            user:
+                users.firstWhereOrNull((element) => element.id == seat.userId),
           );
         },
       ).toList(),
