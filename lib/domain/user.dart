d182a64c9641110192d8fd9a299239d9f18e7ae8
h.kuwabara <h.kuwabara1103@gmail.com>
2023-12-07 09:22:55 +0900

    Userにidプロパティを追加

--- lib/domain/user.dart
+++ lib/domain/user.dart
@@ -7,6 +7,7 @@ part 'user.g.dart';
 @freezed
 class User with _$User {
   const factory User({
+    required String id,
     String? name,
     @Default([]) List<String> skills,
     CareerOption? career,
