4a8e1907c1cff26b540eac24b60178d7778f9a11
h.kuwabara <h.kuwabara1103@gmail.com>
2023-12-17 17:48:36 +0900

    プロフィール画面の画像取得方法を修正

--- lib/feature/profile/ui/component/profile_content.dart
+++ lib/feature/profile/ui/component/profile_content.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:engineer_circle/domain/career_option.dart';
 import 'package:engineer_circle/domain/user.dart';
 import 'package:engineer_circle/gen/assets.gen.dart';
@@ -27,7 +28,8 @@ class ProfileContent extends StatelessWidget {
             profile.avatarUrl != null
                 ? CircleAvatar(
                     radius: iconSize / 2,
-                    backgroundImage: NetworkImage(profile.avatarUrl!),
+                    backgroundImage:
+                        CachedNetworkImageProvider(profile.avatarUrl!),
                   )
                 : Container(
                     width: iconSize,
