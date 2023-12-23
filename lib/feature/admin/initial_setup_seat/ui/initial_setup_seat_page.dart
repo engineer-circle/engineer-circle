db431bbd43aec80e30fd88ba6b4ff1a6d8419154
h.kuwabara <h.kuwabara1103@gmail.com>
2023-12-23 17:47:04 +0900

    フォーカス外してキーボード閉じる処理を追加

--- lib/feature/admin/initial_setup_seat/ui/initial_setup_seat_page.dart
+++ lib/feature/admin/initial_setup_seat/ui/initial_setup_seat_page.dart
@@ -21,117 +21,125 @@ class _InitialSetupSeatPageState extends ConsumerState<InitialSetupSeatPage> {
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(initialSetupSeatStateProvider);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('シート作成'),
-      ),
-      body: Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const Text(
-                  '席の決め方',
-                  style: TextStyle(fontSize: 16),
-                ),
-                const SizedBox(height: 4),
-                DropdownButtonFormField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
+    return GestureDetector(
+      onTap: () {
+        // キーボードが表示されている場合、フォーカスを外してキーボードを閉じる
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('シート作成'),
+        ),
+        body: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Text(
+                    '席の決め方',
+                    style: TextStyle(fontSize: 16),
                   ),
-                  items: SeatSelectionMethod.values
-                      .map((SeatSelectionMethod value) {
-                    return DropdownMenuItem(
-                      value: value,
-                      child: Text(value.label),
-                    );
-                  }).toList(),
-                  onChanged: (SeatSelectionMethod? newValue) => ref
-                      .read(initialSetupSeatStateProvider.notifier)
-                      .updateSeatSelectionMethod(newValue),
-                  value: state.seatSelectionMethod,
-                  validator: (value) => value == null ? '席の決め方を選択してください' : null,
-                ),
-                const SizedBox(height: 16),
-                const Text(
-                  'シート名',
-                  style: TextStyle(fontSize: 16),
-                ),
-                const SizedBox(height: 4),
-                TextFormField(
-                  initialValue: state.seatName,
-                  maxLines: 1,
-                  maxLength: 20,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: (value) => value!.isEmpty ? 'シート名を入力してください' : null,
-                  onChanged: (value) => ref
-                      .read(initialSetupSeatStateProvider.notifier)
-                      .updateSeatName(value),
-                ),
-                const SizedBox(height: 24),
-                FilledButton(
-                  child: const Text(
-                    '作成する',
-                    style: TextStyle(
-                      fontSize: 16,
+                  const SizedBox(height: 4),
+                  DropdownButtonFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
                     ),
+                    items: SeatSelectionMethod.values
+                        .map((SeatSelectionMethod value) {
+                      return DropdownMenuItem(
+                        value: value,
+                        child: Text(value.label),
+                      );
+                    }).toList(),
+                    onChanged: (SeatSelectionMethod? newValue) => ref
+                        .read(initialSetupSeatStateProvider.notifier)
+                        .updateSeatSelectionMethod(newValue),
+                    value: state.seatSelectionMethod,
+                    validator: (value) =>
+                        value == null ? '席の決め方を選択してください' : null,
                   ),
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      _formKey.currentState!.save();
-                      switch (state.seatSelectionMethod!) {
-                        case SeatSelectionMethod.free:
-                          context.router.push(
-                            CreateSeatingChartRoute(
-                              seatTitle: state.seatName!,
-                            ),
-                          );
-                          break;
-                        case SeatSelectionMethod.shuffle:
-                          showDialog(
-                            context: context,
-                            builder: (dialogContext) {
-                              return AlertDialog(
-                                content: const Text(
-                                  'シャッフル方式で座席を作成します',
-                                  style: TextStyle(fontSize: 16),
-                                ),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () =>
-                                        Navigator.of(dialogContext).pop(),
-                                    child: const Text('いいえ'),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'シート名',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(height: 4),
+                  TextFormField(
+                    initialValue: state.seatName,
+                    maxLines: 1,
+                    maxLength: 20,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) =>
+                        value!.isEmpty ? 'シート名を入力してください' : null,
+                    onChanged: (value) => ref
+                        .read(initialSetupSeatStateProvider.notifier)
+                        .updateSeatName(value),
+                  ),
+                  const SizedBox(height: 24),
+                  FilledButton(
+                    child: const Text(
+                      '作成する',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        switch (state.seatSelectionMethod!) {
+                          case SeatSelectionMethod.free:
+                            context.router.push(
+                              CreateSeatingChartRoute(
+                                seatTitle: state.seatName!,
+                              ),
+                            );
+                            break;
+                          case SeatSelectionMethod.shuffle:
+                            showDialog(
+                              context: context,
+                              builder: (dialogContext) {
+                                return AlertDialog(
+                                  content: const Text(
+                                    'シャッフル方式で座席を作成します',
+                                    style: TextStyle(fontSize: 16),
                                   ),
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.of(dialogContext).pop();
-                                      ref
-                                          .read(initialSetupSeatProvider)
-                                          .createShuffleSeat(
-                                            seatName: state.seatName!,
-                                            onSuccess: () {
-                                              context.router.replaceAll(
-                                                [const RootRoute()],
-                                              );
-                                            },
-                                          );
-                                    },
-                                    child: const Text('はい'),
-                                  ),
-                                ],
-                              );
-                            },
-                          );
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () =>
+                                          Navigator.of(dialogContext).pop(),
+                                      child: const Text('いいえ'),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(dialogContext).pop();
+                                        ref
+                                            .read(initialSetupSeatProvider)
+                                            .createShuffleSeat(
+                                              seatName: state.seatName!,
+                                              onSuccess: () {
+                                                context.router.replaceAll(
+                                                  [const RootRoute()],
+                                                );
+                                              },
+                                            );
+                                      },
+                                      child: const Text('はい'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                        }
                       }
-                    }
-                  },
-                ),
-              ],
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
