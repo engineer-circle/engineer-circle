4a8a24a3ba094b6c409ecfafb1cdc7ce5e54e4c1
h.kuwabara <h.kuwabara1103@gmail.com>
2023-12-27 23:31:24 +0900

    削除処理をfirebaseに委譲

--- lib/infrastructure/repository/admin_seating_chart_repository.dart
+++ lib/infrastructure/repository/admin_seating_chart_repository.dart
@@ -1,31 +1,23 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:engineer_circle/domain/create_seating_chart.dart';
-import 'package:engineer_circle/domain/seating_chart.dart';
 import 'package:engineer_circle/infrastructure/remote/firebase.dart';
-import 'package:engineer_circle/infrastructure/remote/firebase_exceptions.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final adminSeatingChartRepositoryProvider = Provider(
   (ref) => AdminSeatingChartRepository(
-    firestore: ref.watch(firebaseFirestoreProvider),
     functions: ref.watch(firebaseFunctionsProvider),
   ),
 );
 
 class AdminSeatingChartRepository {
   AdminSeatingChartRepository({
-    required this.firestore,
     required this.functions,
   });
-
-  final FirebaseFirestore firestore;
   final FirebaseFunctions functions;
 
-  static const seatsCollectionName = 'seats';
-  static const createdAtFieldName = 'createdAt';
   static const createSeatCommand = 'createSeat';
   static const createShuffleSeatCommand = 'createShuffleSeat';
+  static const deleteLatestSeatCommand = 'deleteLatestSeat';
 
   Future<void> createSeatingChart(
     CreateSeatingChart seatingChart,
@@ -45,18 +37,7 @@ class AdminSeatingChartRepository {
     );
   }
 
-  Future<void> deleteRecentSeatingChart() async {
-    final querySnapshot = await firestore
-        .collection(seatsCollectionName)
-        .orderBy(createdAtFieldName, descending: true)
-        .limit(1)
-        .get();
-
-    if (querySnapshot.docs.isNotEmpty) {
-      final mostRecentDoc = querySnapshot.docs.first;
-      await mostRecentDoc.reference.delete();
-    } else {
-      throw FirebaseCustomException('削除するシートがありません');
-    }
+  Future<void> deleteLatestSeatingChart() async {
+    await functions.httpsCallable(deleteLatestSeatCommand).call();
   }
 }
