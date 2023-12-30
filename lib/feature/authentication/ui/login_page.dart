528784e7f833f93ef0e4135f98a3455f1ecef437
h.kuwabara <h.kuwabara1103@gmail.com>
2023-12-30 10:34:29 +0900

    sample spotlite

--- lib/feature/authentication/ui/login_page.dart
+++ lib/feature/authentication/ui/login_page.dart
@@ -1,8 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:engineer_circle/app/router/app_router.dart';
 import 'package:engineer_circle/feature/authentication/controller/authentication_controller.dart';
+import 'package:engineer_circle/feature/authentication/ui/showcase.dart';
+import 'package:engineer_circle/global/logger.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:showcaseview/showcaseview.dart';
 import 'package:sign_in_button/sign_in_button.dart';
 
 @RoutePage()
@@ -14,19 +18,64 @@ class LoginPage extends ConsumerStatefulWidget {
 }
 
 class _LoginState extends ConsumerState<LoginPage> {
+  final GlobalKey _one = GlobalKey();
+  final GlobalKey _two = GlobalKey();
+
+  @override
+  void initState() {
+    startShowCase();
+    super.initState();
+  }
+
+  startShowCase() {
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      ShowCaseWidget.of(context).startShowCase([
+        _one,
+        _two,
+      ]);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('ログイン'),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SizedBox(
+      body: ShowCaseWidget(
+        onStart: (index, key) {
+          logger.i('onStart: $index, $key');
+        },
+        onComplete: (index, key) {
+          logger.i('onComplete: $index, $key');
+          if (index == 4) {
+            SystemChrome.setSystemUIOverlayStyle(
+              SystemUiOverlayStyle.light.copyWith(
+                statusBarIconBrightness: Brightness.dark,
+                statusBarColor: Colors.white,
+              ),
+            );
+          }
+        },
+        blurValue: 1,
+        builder: Builder(builder: (context) => const MailPage()),
+        autoPlayDelay: const Duration(seconds: 3),
+      ),
+    );
+  }
+
+  Widget _body() {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Showcase(
+              key: _one,
+              title: 'Apple',
+              description: 'Apple詳細',
+              child: SizedBox(
                 height: 60,
                 child: SignInButton(
                   Buttons.apple,
@@ -36,8 +85,13 @@ class _LoginState extends ConsumerState<LoginPage> {
                   },
                 ),
               ),
-              const SizedBox(height: 16),
-              SizedBox(
+            ),
+            const SizedBox(height: 16),
+            Showcase(
+              key: _two,
+              title: 'Google',
+              description: 'Google詳細',
+              child: SizedBox(
                 height: 60,
                 child: SignInButton(
                   Buttons.google,
@@ -47,13 +101,13 @@ class _LoginState extends ConsumerState<LoginPage> {
                   },
                 ),
               ),
-              const SizedBox(height: 12),
-              TextButton(
-                onPressed: () => context.router.push(const SignUpRoute()),
-                child: const Text('ご登録がまだの方はこちら'),
-              ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 12),
+            TextButton(
+              onPressed: () => context.router.push(const SignUpRoute()),
+              child: const Text('ご登録がまだの方はこちら'),
+            ),
+          ],
         ),
       ),
     );
