e37fbe2659f2bd404f1b242fe998f526e1f7927b
h.kuwabara <h.kuwabara1103@gmail.com>
2024-01-04 22:23:45 +0900

    Firebaseからユーザー権限を取得する処理を追加

--- lib/infrastructure/repository/admin_seating_chart_repository.dart
+++ lib/infrastructure/repository/admin_seating_chart_repository.dart
@@ -1,28 +1,44 @@
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:engineer_circle/domain/create_seating_chart.dart';
 import 'package:engineer_circle/infrastructure/remote/firebase.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final adminSeatingChartRepositoryProvider = Provider(
   (ref) => AdminSeatingChartRepository(
     functions: ref.watch(firebaseFunctionsProvider),
+    firebaseAuth: ref.watch(firebaseAuthProvider),
   ),
 );
 
 class AdminSeatingChartRepository {
   AdminSeatingChartRepository({
-    required this.functions,
-  });
-  final FirebaseFunctions functions;
+    required FirebaseFunctions functions,
+    required FirebaseAuth firebaseAuth,
+  })  : _functions = functions,
+        _firebaseAuth = firebaseAuth;
+
+  final FirebaseFunctions _functions;
+  final FirebaseAuth _firebaseAuth;
 
   static const createSeatCommand = 'createSeat';
   static const createShuffleSeatCommand = 'createShuffleSeat';
   static const deleteLatestSeatCommand = 'deleteLatestSeat';
 
+  Future<bool> checkUserRole() async {
+    final user = _firebaseAuth.currentUser;
+    if (user != null) {
+      final idTokenResult = await user.getIdTokenResult();
+      return idTokenResult.claims?['admin'] == true;
+    } else {
+      return false;
+    }
+  }
+
   Future<void> createSeatingChart(
     CreateSeatingChart seatingChart,
   ) async {
-    await functions.httpsCallable(createSeatCommand).call(
+    await _functions.httpsCallable(createSeatCommand).call(
       <String, dynamic>{
         'seatingChart': seatingChart.toJson(),
       },
@@ -30,7 +46,7 @@ class AdminSeatingChartRepository {
   }
 
   Future<void> createShuffleSeatingChart(String seatTitle) async {
-    await functions.httpsCallable(createShuffleSeatCommand).call(
+    await _functions.httpsCallable(createShuffleSeatCommand).call(
       <String, dynamic>{
         'seatTitle': seatTitle,
       },
@@ -38,6 +54,6 @@ class AdminSeatingChartRepository {
   }
 
   Future<void> deleteLatestSeatingChart() async {
-    await functions.httpsCallable(deleteLatestSeatCommand).call();
+    await _functions.httpsCallable(deleteLatestSeatCommand).call();
   }
 }
